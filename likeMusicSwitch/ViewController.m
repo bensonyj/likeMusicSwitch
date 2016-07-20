55f78bf97964134e9342a111e98355494a7e1e9c
bensonyj <923427954@qq.com>
2016-07-20 23:38:57 +0800

    添加音乐封面切换效果(未完成)

--- likeMusicSwitch/ViewController.m
+++ likeMusicSwitch/ViewController.m
@@ -9,6 +9,13 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    NSInteger currentIndex;
+}
+
+@property (weak, nonatomic) IBOutlet UIView *contentView;
+
+@property (strong, nonatomic) NSArray *imageArray;
 
 @end
 
@@ -17,6 +24,49 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self addLayer];
+}
+
+- (void)addLayer
+{
+    for (NSInteger i = 0, count = [self.imageArray count]; i < count; i++) {
+        CALayer *layer = [CALayer layer];
+        layer.frame = self.contentView.bounds;
+        layer.contents = (__bridge id _Nullable)([UIImage imageNamed:self.imageArray[i]].CGImage);
+        layer.contentsScale = [UIScreen mainScreen].scale;
+        layer.contentsGravity = kCAGravityResizeAspect;
+        [self.contentView.layer addSublayer:layer];
+    }
+}
+
+- (IBAction)beforeClick:(id)sender {
+    if (currentIndex == 1) {
+        return;
+    }
+    
+    //当前图层右移动旋转，上一张图层左移动旋转
+//    CALayer *currentLayer = [self.contentView.layer ]
+//    CABasicAnimation *animation = [CABasicAnimation animation];
+//    animation.keyPath = @"";
+//    animation.toValue = @"";
+//    animation.duration = 0.5;
+    
+}
+
+- (IBAction)afterClick:(id)sender {
+    if (currentIndex == [self.imageArray count]) {
+        return;
+    }
+}
+
+- (NSArray *)imageArray
+{
+    if (!_imageArray) {
+        _imageArray = @[@"1",@"2",@"3"];
+    }
+    
+    return _imageArray;
 }
 
 - (void)didReceiveMemoryWarning {
